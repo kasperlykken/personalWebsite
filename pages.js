f351ea5dbed9db09333953b8c3652e30cde0637a
kasperlykken <74990700+kasperlykken@users.noreply.github.com>
2026-08-11 00:21:55 +0200

    Update D&D link to point to the new website directory

--- pages.js
+++ pages.js
@@ -30,7 +30,7 @@ window.HUB_CONFIG = {
     {
       title: "D&D",
       description: "Kampagnesider, handouts, wiki og rollespilsmateriale.",
-      href: "./dnd/",
+      href: "./dnd/website/",
       category: "D&D",
       icon: "D",
       badge: ""
